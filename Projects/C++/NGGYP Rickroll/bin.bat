bccab4330dc88e08292bdca0f74b5eb2e9317a4b
arsche <xentai001.yt@gmail.com>
2022-11-04 09:46:45 +0200

    NGGYP: hotfix
    - "libstdc++-6.dll not found" fixed

--- Projects/C++/NGGYP Rickroll/bin.bat
+++ Projects/C++/NGGYP Rickroll/bin.bat
@@ -1,6 +1,6 @@
 @echo off
 echo compile c++ to bin/windows
-g++ main.cpp -o bin/windows/nggyp.exe
+g++ main.cpp -static-libgcc -static-libstdc++ -o bin/windows/nggyp.exe
 
 echo.
 echo Press any key to close . . .
